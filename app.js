a38344451b6f7e89352b920254aff051d8fe9e9f
yostto-leal <lealariosto@gmail.com>
2024-11-04 08:33:20 -0300

    perf:mudanca codigo performace

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 function consultaEndereco(){
+    let cep = document.querySelector('#cep').value ;
+     
+    if (cep.length !== 8){
+        alert('CEP Invalido !!');
+        return
+    }
+     
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
 
-    let url = `https://viacep.com.br/ws/${cep}/json/ `
+    fetch (url).then(function(response ){
+        response.json().then(mostrarEndereco);
+          
+        })
+        
+    
 
-    fetch (url).then(function(response) {
-        response.json().then(mostraEndereco)
-    })
 }
 
-function mostraEndereco(dados){
+ function mostrarEndereco(dados){
     let resultado = document.querySelector('#resultado')
     if (dados.erro){
-        resultado.innerHTML = " Não foi possivel localizar o endereço !!"
-    }else{
+        resultado.innerHTML = "Nao foi possivel localizar endereco!!"
+    }else {
         resultado.innerHTML = `<p>Endereco: ${dados.logradouro} </p>
-        <p>Complemento: ${dados.complemento}</p>
-        <p>Bairro: ${dados.bairro}</p> 
-        <p>Cidade: ${dados.localidade}- ${dados.uf}</p>
-        <p>Estado: ${dados.estado}</p>
-        <p>ddd: ${dados.ddd}</p>`
-}
-
+                              <p>Complemento: ${dados.complemento}</p>
+                              <p>Bairro: ${dados.bairro}</p> 
+                              <p>Cidade: ${dados.localidade}- ${dados.uf}</p>
+                              <p>Estado: ${dados.estado}</p>
+                              <p>ddd: ${dados.ddd}</p>`
+    }
+     
     
-}
+
+
+ }
